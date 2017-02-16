cf03f0aa83173757e73c13873fcae789b2b66a20
Itay Boneh <itay.boneh@eyesight-tech.com>
2017-02-16 23:33:25 +0200

    Separated gradient calculation and update

--- binary_connect.py
+++ binary_connect.py
@@ -356,9 +356,14 @@ def training(loss, learning_rate):
     # optimizer = tf.train.AdamOptimizer(learning_rate)
     # Create a variable to track the global step.
     global_step = tf.Variable(0, name='global_step', trainable=False)
-    # Use the optimizer to apply the gradients that minimize the loss
-    # (and also increment the global step counter) as a single training step.
-    train_op = optimizer.minimize(loss, global_step=global_step)
+    # # Use the optimizer to apply the gradients that minimize the loss
+    # # (and also increment the global step counter) as a single training step.
+    # train_op = optimizer.minimize(loss, global_step=global_step)
+    # Use the optimizer to compute gradients
+    grads_and_vars = optimizer.compute_gradients(loss, var_list=tf.trainable_variables())
+    # vars_with_grad = [v for g, v in grads_and_vars if g is not None]
+    # Apply gradient updates
+    train_op = optimizer.apply_gradients(grads_and_vars=grads_and_vars, global_step=global_step)
     return train_op
 
 
